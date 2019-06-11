672f772dc7f35726898a4132b436329effbf81e3
Kathryn Rees <karees@ncsu.edu>
2019-06-11 15:32:50 -0400

    more onchange removal

--- typings/SimpleTimeWidgetProps.d.ts
+++ typings/SimpleTimeWidgetProps.d.ts
@@ -4,8 +4,7 @@
  * @author Mendix Widgets Team
  */
 import { CSSProperties } from "react";
-import { pages } from "mendixmodelsdk";
-import { ActionValue, EditableValue } from "@mendix/pluggable-widgets-api/properties";
+import { EditableValue } from "@mendix/pluggable-widgets-api/properties";
 
 interface CommonProps {
     id: string;
@@ -19,17 +18,14 @@ export type EditableEnum = "default" | "never";
 export interface SimpleTimeWidgetContainerProps extends CommonProps {
     timeAttribute: EditableValue<Date>;
     editable: EditableEnum;
-    onChangeAction?: ActionValue;
 }
 
 export interface SimpleTimeWidgetPreviewProps extends CommonProps {
     timeAttribute: string;
     editable: EditableEnum;
-    onChangeAction?: pages.ClientAction;
 }
 
 export interface VisibilityMap {
     timeAttribute: boolean;
     editable: boolean;
-    onChangeAction: boolean;
 }
